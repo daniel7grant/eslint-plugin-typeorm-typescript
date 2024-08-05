7a74ad34f98a09f9a14aca0e1a55e36fa4eb70a2
Grant <gergely.daniel7@gmail.com>
2024-08-06 00:58:16 +0200

    Add one more test for array test

--- src/rules/enforce-column-types.test.ts
+++ src/rules/enforce-column-types.test.ts
@@ -159,14 +159,14 @@ ruleTester.run('enforce-column-types', enforceColumnTypes, {
             }`,
         },
         {
-            name: 'should ignore unknown types',
+            name: 'should allow unknown types',
             code: `class Entity {
                 @Column({ type: 'json' })
                 unknownField: unknown;
             }`,
         },
         {
-            name: 'should ignore unknown array types',
+            name: 'should allow unknown array types',
             code: `class Entity {
                 @Column({ type: 'json' })
                 unknownArray: unknown[];
@@ -186,6 +186,14 @@ ruleTester.run('enforce-column-types', enforceColumnTypes, {
             class Entity {
                 @Column({ type: 'json', nullable: true })
                 referenceNullable: JsonObject | null;
+            }`,
+        },
+        {
+            name: 'should allow reference array types',
+            code: `type JsonObject = {};
+            class Entity {
+                @Column({ type: 'jsonb' })
+                referenceArray: JsonObject[];
             }`,
         },
         {
