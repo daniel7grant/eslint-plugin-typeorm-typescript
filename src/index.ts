eaea71d40bf61e8e05d0fe6a4fb41044142063fc
Ludwig Meysel <lmeysel@users.noreply.github.com>
2024-10-18 13:47:17 +0200

    Register enforce-relation-wrapper rule

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import { FlatConfig } from '@typescript-eslint/utils/ts-eslint';
 import enforceColumnTypes from './rules/enforce-column-types.js';
 import enforceConsistentNullability from './rules/enforce-consistent-nullability.js';
 import enforceRelationTypes from './rules/enforce-relation-types.js';
+import enforceRelationWrapper from './rules/enforce-relation-wrapper.js';
 import recommended from './recommended.js';
 
 export const rules = {
     'enforce-column-types': enforceColumnTypes,
     'enforce-consistent-nullability': enforceConsistentNullability,
     'enforce-relation-types': enforceRelationTypes,
+    'enforce-relation-wrapper': enforceRelationWrapper,
 };
 
 export const plugin: FlatConfig.Plugin = {
