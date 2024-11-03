230879108a4afd952331df2efd200bbc87bcfb1f
Grant <gergely.daniel7@gmail.com>
2024-11-03 16:59:07 +0100

    Add relation to examples

--- examples/recommended/index.ts
+++ examples/recommended/index.ts
@@ -1,7 +1,22 @@
-import { Entity, Column } from 'typeorm';
+import { Entity, Column, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity({ name: 'User' })
+@Entity()
+export class Post {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ type: 'number' })
+    userId: number;
+}
+
+@Entity()
 export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
     @Column({ type: 'string' })
     name: number;
+
+    @OneToMany(() => Post, (post) => post.userId)
+    posts: Post;
 }
